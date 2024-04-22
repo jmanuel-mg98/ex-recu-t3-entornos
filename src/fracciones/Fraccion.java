2d177f70ff018c8246da0c34e29a4947ebf02b73
jmanuel-mg98 <148326871+jmanuel-mg98@users.noreply.github.com>
2024-04-22 10:46:21 +0200

    subida de los cambios en la rama documentacion

--- src/fracciones/Fraccion.java
+++ src/fracciones/Fraccion.java
@@ -1,11 +1,27 @@
 package fracciones;
+/**
+ * la clase fraccion guardara todos los metodos relaciondo con las funciones que pueden
+ * hacer las fracciones 
+ * 
+ * @author josem.martin
+ * 
+ */
 
 public class Fraccion {
+	/**
+	 * numerador de la fraccion
+	 */
 	private int num;
+	/**
+	 * denominador de la fraccion
+	 */
 	private int den;
 	
 	
 	//Constructor por defecto
+	/**
+	 * es el contructor por defecto que genera valores neutros 
+	 */
 	
 	public Fraccion() { //debe generar el valor neutro que es 0/1
 		num=0;
@@ -13,37 +29,64 @@ public class Fraccion {
 		
 	}
 	
-	//Constructor con parámetros
+	//Constructor con parï¿½metros
+	/**
+	 * aqui tenemos el constructor por parametros que recibiÃ¡
+	 * @param num
+	 * @param den
+	 */
 	public Fraccion (int num,int den) {
 		//debo evitar crear fracciones con 0 en el divisor
 		this.num=num;
 		if(den==0)
 			den=1;
 		this.den=den;
-		//devuelvo la fracción simplificada
+		//devuelvo la fracciï¿½n simplificada
 		simplificar();
 	}
 	
 	//getters y setters
+	/**
+	 * a continuacion vienen los set para comprobar los nummeros son correctos
+	 * @param num
+	 */
 	
 	public void setNum(int num) {
 		this.num=num;
 		
 	}
-	
+	/**
+	 * aqui encontramos el set con el numero ya correcto
+	 * 
+	 * @return num
+	 */
 	public int getNum() {
 		return num;
 		
 	}
-	
+	/**
+	 * ahora encontramos los mismos pero con los num del denominador
+	 * 
+	 * @param den
+	 */
 	public void setDen(int den) {
 		this.den=den;
 	}
+	/**
+	 * al igual que con los numeradores devuelve ya el denominador bien
+	 * 
+	 * @return den
+	 */
 	public int getDen() {
 		return den;
 	}
 	
-	 //Cálculo del máximo común divisor por el algoritmo de Euclides                                               
+	 //Cï¿½lculo del mï¿½ximo comï¿½n divisor por el algoritmo de Euclides  
+	/**
+	 * calcula el mcd por el algoritmo de Euclides 
+	 * 
+	 * @return minimo
+	 */
     private int mcd() {
         int maximo = Math.max(Math.abs(num), Math.abs(den));
         int minimo = Math.min(Math.abs(num), Math.abs(den));
@@ -58,15 +101,22 @@ public class Fraccion {
         return minimo;
     }
     
-    //método para simplificar fracciones
+    //mï¿½todo para simplificar fracciones
+    /**
+     * este metodo sirve para simplificar las fracciones 
+     */
     public void simplificar() {
     	int n=mcd();
     	num=num/n;
     	den=den/n;
     }
     
-    //Método toString
-    
+    //Mï¿½todo toString
+    /**
+     * en este metodo imprimiremos ya la fraccion 
+     * 
+     * @return mensaje
+     */
     public String toString() {
     	String mensaje = num+ "/"+den;
     	return mensaje;
@@ -75,6 +125,12 @@ public class Fraccion {
     
     
     //Sumar fracciones
+    /**
+     * este metodo sirve para sumar fracciones 
+     * 
+     * @param f
+     * @return aux
+     */
     
     public Fraccion sumar(Fraccion f) {
     	Fraccion aux= new Fraccion();
@@ -86,6 +142,12 @@ public class Fraccion {
     }
     
     //Restar fracciones
+    /**
+     * este metodo sirve para restar fracciones
+     * 
+     * @param f
+     * @return aux
+     */
     
     public Fraccion restar(Fraccion f) {
     	Fraccion aux=new Fraccion();
@@ -98,7 +160,12 @@ public class Fraccion {
     }
     
 //Multiplicar fracciones
-    
+    /**
+     * este metodo sirfe para multiplicar fracciones 
+     * 
+     * @param f
+     * @return aux
+     */
     public Fraccion multiplicar(Fraccion f) {
     	Fraccion aux=new Fraccion();
     	aux.num=num*f.num;
@@ -109,6 +176,12 @@ public class Fraccion {
     }
     
 //Dividir fracciones
+    /**
+     * este metodo te realiza la division de las fracciones 
+     * 
+     * @param f
+     * @return aux
+     */
     
     public Fraccion dividir(Fraccion f) {
     	Fraccion aux=new Fraccion();
